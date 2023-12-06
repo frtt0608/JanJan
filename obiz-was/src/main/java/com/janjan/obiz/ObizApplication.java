7bed5aed50000d21872d03212f4c6cbc1d7ec32f
frtt0608 <frtt0608@gmail.com>
2023-12-06 15:00:04 +0900

    Feat: spring boot3과 DB 연동
    mysql/obiz, mongoDB 연동 및 빌드 성공 확인
    추가적으로 mongoDB에 admin계정 인증과 JPA기반으로 connect 확인 필요

---
+++ obiz-was/src/main/java/com/janjan/obiz/ObizApplication.java
@@ -0,0 +1,13 @@
+package com.janjan.obiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObizApplication.class, args);
+	}
+
+}
---
+++ obiz-was/src/test/java/com/janjan/obiz/ObizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janjan.obiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
